17cf7dddff1633a350742ba8d5082c352e363d57
Nahuel Araujo <nahuelaraujo1991@gmail.com>
2024-02-22 20:15:55 +0100

    Delete Web 1 - Bootstrap - JavaScript directory

--- Web 1 - Bootstrap - JavaScript/Sitio web/js/slick_marcas.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.slick-marcas').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-    });
-})
